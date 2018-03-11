8e065c32983b1cbac6b14d057c6b9a36522b24fc
Amedeo Amato <amato.amedeo@gmail.com>
2018-03-11 19:31:05 +0100

    add aliexpress api function to get details of arbitrary product

--- telegram_aliexpress_bot/aliexpress_api.py
+++ telegram_aliexpress_bot/aliexpress_api.py
@@ -8,6 +8,8 @@ from telegram_aliexpress_bot.rebrandly_api import RebrandlyApi
 
 
 class AliExpressApi(object):
+    API_URL_BASE = 'http://gw.api.alibaba.com/openapi/param2/2/portals.open/'
+
     def __init__(self):
         self._read_config()
 
@@ -20,59 +22,66 @@ class AliExpressApi(object):
         self._appkey = config.get('AliExpress', 'appkey')
         self._tracking_id = config.get('AliExpress', 'trackingId')
 
-    @staticmethod
-    def _query_json_api(url, params):
+    def _query_json_api(self, url, params):
         """Queries a JSON API"""
         response = requests.get(url=url, params=params)
         data = json.loads(response.text)
         return data
 
-    def get_promotion_products(self, keyword):
+    def get_promotion_products(self, keyword=''):
         """Gets currently promoted products on AliExpress"""
-        promotion_product_url = "http://gw.api.alibaba.com/openapi/param2/2/portals.open/api.listPromotionProduct/" \
-                                + self._appkey
+        promotion_product_url = self.API_URL_BASE + 'api.listPromotionProduct/' + self._appkey
         params = dict(
-            fields="productTitle,productUrl,imageUrl,30daysCommission,salePrice",
+            fields='productTitle,productUrl,imageUrl,30daysCommission,salePrice',
             keywords=keyword,
-            sort="volumeDown"
+            sort='volumeDown'
         )
         return self._query_json_api(promotion_product_url, params)
 
+    def get_promotion_product_detail(self, product_id):
+        """Gets details for the product with the given product id"""
+        promotion_product_detail_url = self.API_URL_BASE + 'api.getPromotionProductDetail/' + self._appkey
+        params = dict(
+            fields='productTitle,productUrl,imageUrl,30daysCommission,salePrice',
+            productId=product_id
+        )
+        return self._query_json_api(promotion_product_detail_url, params)
+
     class Category(Enum):
         """Categories to be used with  get_hot_products()"""
-        All = "",
-        Apparel = "3",
-        Automobiles = "34",
-        Beauty = "66",
-        Computer = "7",
-        Home = "13",
-        Electronics = "	44",
-        ElectricalEquipment = "5",
-        Food = "2",
-        Furniture = "1503",
-        Hair = "200003655",
-        Hardware = "42",
-        Garden = "15",
-        Appliances = "6",
-        Industry = "200001996",
-        Jewelry = "36",
-        Lights = "39",
-        Luggage = "1524",
-        Mother = "1501",
-        Office = "21",
-        Phones = "509",
-        Security = "30",
-        Shoes = "322",
-        Sports = "18",
-        Tools = "1420",
-        Toys = "26",
-        Travel = "200003498",
-        Watches = "1511",
-        Weddings = "320"
+        All = '',
+        Apparel = '3',
+        Automobiles = '34',
+        Beauty = '66',
+        Computer = '7',
+        Home = '13',
+        Electronics = '	44',
+        ElectricalEquipment = '5',
+        Food = '2',
+        Furniture = '1503',
+        Hair = '200003655',
+        Hardware = '42',
+        Garden = '15',
+        Appliances = '6',
+        Industry = '200001996',
+        Jewelry = '36',
+        Lights = '39',
+        Luggage = '1524',
+        Mother = '1501',
+        Office = '21',
+        Phones = '509',
+        Security = '30',
+        Shoes = '322',
+        Sports = '18',
+        Tools = '1420',
+        Toys = '26',
+        Travel = '200003498',
+        Watches = '1511',
+        Weddings = '320'
 
     def get_hot_products(self, category=Category.All):
         """Gets currently hot products on AliExpress"""
-        hot_product_url = "https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.listHotProducts/" + self._appkey
+        hot_product_url = self.API_URL_BASE + 'api.listHotProducts/' + self._appkey
         params = dict(
             categoryId=category.value
         )
@@ -80,17 +89,24 @@ class AliExpressApi(object):
 
     def get_promotion_link(self, product_link):
         """Transforms a regular link into a promotion / referral link"""
-        promotion_link_url = "http://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getPromotionLinks/"\
-                             + self._appkey
+        promotion_link_url = self.API_URL_BASE + 'api.getPromotionLinks/' + self._appkey
+
         params = dict(
             trackingId=self._tracking_id,
             urls=product_link,
         )
-        result = self._query_json_api(promotion_link_url, params)["result"]
-        return result["promotionUrls"][0]["promotionUrl"]
-    
+        result = self._query_json_api(promotion_link_url, params)['result']
+        return result['promotionUrls'][0]['promotionUrl']
+
     def get_short_promotion_link(self, product_link):
         """Gets a shortened promotion link"""
         link = self.get_promotion_link(product_link)
         shortener = RebrandlyApi()
-        return shortener.get_short_url(link, "AliExpress_")
+        return shortener.get_short_url(link, 'AliExpress_')
+
+    @staticmethod
+    def get_product_id_from_link(link):
+        """Returns the product id from a regular AliExpress link"""
+        import re
+        match = re.search(r'.*\.aliexpress\.com/item/.*/(\d*)\.html.*', link)
+        return match.group(1)
--- telegram_aliexpress_bot/test/test_aliexpress_api.py
+++ telegram_aliexpress_bot/test/test_aliexpress_api.py
@@ -8,8 +8,15 @@ class TestAliExpressApi(unittest.TestCase):
         self.cut = AliExpressApi()
 
     def test_get_promotion_products(self):
-        data = self.cut.get_promotion_products("something")
+        data = self.cut.get_promotion_products('something')
         self._verify_product_results(data)
+
+    def test_get_promotion_product_detail(self):
+        data = self.cut.get_promotion_product_detail(32719809585)
+        self.assertTrue('https://' in data['result']['imageUrl'])
+        self.assertTrue('https://' in data['result']['productUrl'])
+        self.assertTrue(data['result']['productTitle'] != '')
+        self.assertTrue(data['result']['salePrice'] != '')
 
     def test_get_hot_products(self):
         data = self.cut.get_hot_products(AliExpressApi.Category.Hardware)
@@ -20,22 +27,31 @@ class TestAliExpressApi(unittest.TestCase):
         self._verify_product_results(data)
 
     def _verify_product_results(self, data):
-        self.assertTrue(data["result"]["totalResults"] > 0)
-        self.assertTrue("https://" in data["result"]["products"][0]["imageUrl"])
-        self.assertTrue("https://" in data["result"]["products"][0]["productUrl"])
-        self.assertTrue(data["result"]["products"][0]["productTitle"] != "")
-        self.assertTrue(data["result"]["products"][0]["salePrice"] != "")
+        self.assertTrue(data['result']['totalResults'] > 0)
+        self.assertTrue('https://' in data['result']['products'][0]['imageUrl'])
+        self.assertTrue('https://' in data['result']['products'][0]['productUrl'])
+        self.assertTrue(data['result']['products'][0]['productTitle'] != '')
+        self.assertTrue(data['result']['products'][0]['salePrice'] != '')
 
     def test_get_promotion_link(self):
-        product_link = self.cut.get_hot_products()["result"]["products"][0]["productUrl"]
+        product_link = self.cut.get_hot_products()['result']['products'][0]['productUrl']
         link = self.cut.get_promotion_link(product_link)
-        self.assertTrue("http://" in link)
+        self.assertTrue('http://' in link)
 
     def test_get_short_promotion_link(self):
         # TODO: Mock Rebrandly API
-        product_link = self.cut.get_hot_products()["result"]["products"][0]["productUrl"]
+        product_link = self.cut.get_hot_products()['result']['products'][0]['productUrl']
         link = self.cut.get_short_promotion_link(product_link)
-        self.assertTrue("rebrand.ly/AliExpress_" in link)
+        self.assertTrue('rebrand.ly/AliExpress_' in link)
+
+    def test_get_product_id_from_link(self):
+        product_id = self.cut.get_product_id_from_link('https://de.aliexpress.com/item/WWWOOR-Fashion-Luxury-brand-Watc'
+                                                       'hes-men-Stainless-Steel-Mesh-strap-Quartz-watch-Ultra-Thin-Dial'
+                                                       '-Clock/32719809585.html?aff_platform=product&cpt=1520787671494&'
+                                                       'sk=3fEmAQz&aff_trace_key=f6618482534346b7bd9d6345c749ea51-15207'
+                                                       '87671494-08292-3fEmAQz&terminal_id=f23f16b363201aa7ba3048638953'
+                                                       '5c09')
+        self.assertEqual('32719809585', product_id)
 
 if __name__ == '__main__':
     unittest.main()
